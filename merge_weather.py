7bf29e69a9d178b4c25001db6a337760da9d2037
Weihao Sun <sunweihao2019@163.com>
2023-11-01 22:16:55 -0700

    merged and calculate daily weather

---
+++ merge_weather.py
@@ -0,0 +1,30 @@
+import pandas as pd 
+
+# weather_1 = pd.read_csv('climate-daily/climate-daily (1).csv')
+# print(weather_1.head)
+
+# Read all the weather data
+weather_df = pd.read_csv('climate-daily/climate-daily (1).csv')
+for i in range(2,26):
+    path = 'climate-daily/climate-daily ('+str(i)+').csv'
+    # print(path)
+    sub_df = pd.read_csv(path)
+    weather_df = weather_df._append(sub_df)
+
+# print(weather_df.shape)
+
+# print(weather_df['LOCAL_DATE'])
+weather_df = weather_df.groupby('LOCAL_DATE')[['MIN_TEMPERATURE','MEAN_TEMPERATURE','MAX_TEMPERATURE',
+    'TOTAL_SNOW','TOTAL_RAIN','TOTAL_PRECIPITATION','HEATING_DEGREE_DAYS','COOLING_DEGREE_DAYS']].mean().reset_index()
+
+weather_df['LOCAL_DATE'] = pd.to_datetime(weather_df['LOCAL_DATE'])
+print(weather_df.head(20))
+print(weather_df.shape)
+
+# Output weather data (no need to run again anymore)
+# weather_df.to_csv('data/weather.csv')
+
+# Output merged weather data
+weather_df.to_csv('data/weather_merged.csv')
+
+
