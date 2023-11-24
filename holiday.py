11e4043077caa5a8ec0e1873a898f0f9137a51d0
Felix Fu <fyx@ubcprivate70.ece.ubc.ca>
2023-11-23 23:53:04 -0800

    resolving conflicts

--- holiday.py
+++ holiday.py
@@ -4,12 +4,12 @@ import datetime as dt
 import holidays
 
 ### Build up the holiday features in BC 
-def holiday_feature(df,province):
-    bc_holidays = holidays.country_holidays('CA', subdiv =province)
+def holiday_feature_onehot(df):
+    bc_holidays = holidays.country_holidays('CA', subdiv ='BC')
     df['is_holiday'] = df['Date'].map(lambda x: 1 if x in bc_holidays else 0)
     df['holiday_name'] = df['Date'].map(lambda x: bc_holidays.get(x) if x in bc_holidays else 'normal day')
-    # rated_dummies = pd.get_dummies(df['holiday_name'], dtype=int)
-    # df = pd.concat([df, rated_dummies], axis=1)
+    rated_dummies = pd.get_dummies(df['holiday_name'], dtype = int)
+    df = pd.concat([df, rated_dummies], axis=1)
     return df 
 
 def weekend_feature(df):
